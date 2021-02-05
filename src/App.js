d65e59407b2f14438d3c26e8a8a1841f56e62454
Abou Bkar Siddiq <aboubkar786@gmail.com>
2021-02-05 15:50:40 +0500

    wow

--- src/App.js
+++ src/App.js
@@ -2,28 +2,13 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import Button from 'react-bootstrap/Button'
+import File from './file';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Northbridge</h1>
-      <button className='btn btn-primary btn-lg'> Guru </button>
-      <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle"
-          type="button" id="dropdownMenu1" data-toggle="dropdown"
-          aria-haspopup="true" aria-expanded="false">
-          Dropdown
-      </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenu1">
-          <a class="dropdown-item" href="#!">Action</a>
-          <a class="dropdown-item" href="#!">Another action</a>
-        </div>
-      </div>
-      <div class="alert alert-success" role="alert">
-        <strong>Well done!</strong> You successfully read this
-        important alert message.
-      </div>
+      <File />      
     </div>
   );
 }
---
+++ src/file.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+export default function File() {
+  return (
+    <div>
+      <h1>Hello Northbridge</h1>
+      <button className='btn btn-primary btn-lg'> Guru </button>
+      <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle"
+          type="button" id="dropdownMenu1" data-toggle="dropdown"
+          aria-haspopup="true" aria-expanded="false">
+          Dropdown
+      </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenu1">
+          <a class="dropdown-item" href="#!">Action</a>
+          <a class="dropdown-item" href="#!">Another action</a>
+        </div>
+      </div>
+      <div class="alert alert-success" role="alert">
+        <strong>Well done!</strong> You successfully read this
+        important alert message.
+      </div>
+    </div>
+  )
+}
